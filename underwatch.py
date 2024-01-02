994da9efeacbe9d440aa825eacb1dd4335659ce3
grianon <anongrits@gmail.com>
2024-01-02 13:36:32 -0800

    add log to file, hoping to catch crash

--- underwatch.py
+++ underwatch.py
@@ -1,4 +1,5 @@
 import sys
+import logging
 import asyncio
 
 from PyQt5.QtWidgets import *
@@ -8,6 +9,14 @@ from PyQt5.QtGui import *
 from computer_vision import ComputerVision
 from overlay import Overlay
 from device_control import DeviceControlWidget
+
+logging.basicConfig(
+    filename="logs.log",
+    filemode="w",
+    format="%(name)s - %(levelname)s - %(message)s",
+    level=logging.DEBUG,
+)
+
 
 class GUI(QMainWindow):
     def __init__(self) -> None:
@@ -28,7 +37,7 @@ class GUI(QMainWindow):
         self.computer_vision = ComputerVision()
         self.overlay = Overlay(self.computer_vision)
         self.overlay.show()
-        self.setup_tabs();
+        self.setup_tabs()
         self.show()
 
         self.backgroud_thread = Worker(self.background_thread_loop)
@@ -49,14 +58,19 @@ class GUI(QMainWindow):
             await self.update()
 
     async def update(self):
-        QApplication.processEvents()
-        QGuiApplication.processEvents()
-        QCoreApplication.processEvents()
-        await asyncio.sleep(5/1000)
-        self.computer_vision.update()
-        self.underwatch_tab.update()
-        self.overlay.update()
-        await self.device_control.update(self.computer_vision.get_current_score())
+        try:
+            QApplication.processEvents()
+            QGuiApplication.processEvents()
+            QCoreApplication.processEvents()
+            await asyncio.sleep(5 / 1000)
+            self.computer_vision.update()
+            self.underwatch_tab.update()
+            self.overlay.update()
+            await self.device_control.update(self.computer_vision.get_current_score())
+        except Exception as e:
+            logging.error("Error during update: %s", str(e))
+            raise
+
 
 class UnderwatchTab(QWidget):
     def __init__(self, parent, computer_vision, overlay) -> None:
@@ -73,16 +87,16 @@ class UnderwatchTab(QWidget):
         scroll_area.setFrameShape(QFrame.NoFrame)
         scroll_area.setWidget(scroll_widget)
         scroll_area.setWidgetResizable(True)
-        
+
         outer_layout = QGridLayout()
-        outer_layout.setContentsMargins(0,0,0,0)
+        outer_layout.setContentsMargins(0, 0, 0, 0)
         outer_layout.addWidget(scroll_area)
         self.setLayout(outer_layout)
 
         row = 0
 
         show_overlay_mode_label = QLabel("Show Overlay:", self)
-        inner_layout.addWidget(show_overlay_mode_label, row,0)
+        inner_layout.addWidget(show_overlay_mode_label, row, 0)
         show_overlay_mode = QComboBox(self)
         inner_layout.addWidget(show_overlay_mode, row, 1)
         show_overlay_mode.addItem("Never")
@@ -93,13 +107,15 @@ class UnderwatchTab(QWidget):
         row += 1
 
         show_detection_regions_label = QLabel("Debug Detection Regions:", self)
-        inner_layout.addWidget(show_detection_regions_label, row,0)
+        inner_layout.addWidget(show_detection_regions_label, row, 0)
         show_detection_regions_mode = QComboBox(self)
         inner_layout.addWidget(show_detection_regions_mode, row, 1)
         show_detection_regions_mode.addItem("Never")
         show_detection_regions_mode.addItem("Always")
         show_detection_regions_mode.addItem("When Detection Occurs")
-        show_detection_regions_mode.currentIndexChanged.connect(overlay.update_show_regions_mode)
+        show_detection_regions_mode.currentIndexChanged.connect(
+            overlay.update_show_regions_mode
+        )
         show_detection_regions_mode.setCurrentIndex(overlay.show_regions_mode)
         row += 1
 
@@ -114,7 +130,9 @@ class UnderwatchTab(QWidget):
         ignore_redundant_label = QLabel("Ignore Redundant Assits", self)
         ignore_redundant_box = QCheckBox(self)
         ignore_redundant_box.setChecked(computer_vision.ignore_redundant_assists)
-        ignore_redundant_box.stateChanged.connect(computer_vision.set_ignore_redundant_assists)
+        ignore_redundant_box.stateChanged.connect(
+            computer_vision.set_ignore_redundant_assists
+        )
         inner_layout.addWidget(ignore_redundant_label, row, 0)
         inner_layout.addWidget(ignore_redundant_box, row, 1)
         row += 1
@@ -127,7 +145,7 @@ class UnderwatchTab(QWidget):
         inner_layout.addWidget(score_label, row, 0)
         inner_layout.addWidget(self.score_input_box, row, 1)
         row += 1
-        
+
         decay_label = QLabel("Score Decay Per Minute", self)
         decay_input_box = QSpinBox(self)
         decay_input_box.setMaximum(999)
@@ -146,7 +164,7 @@ class UnderwatchTab(QWidget):
         detectables_group.setLayout(detectables_layout)
         inner_layout.addWidget(detectables_group, row, 0, 1, 2)
         for item in computer_vision.detectables.items():
-            if (item[0] == "KillcamOrPOTG"):
+            if item[0] == "KillcamOrPOTG":
                 continue
             detectable = UnderwatchTab.DetectableWidget(self, item)
             detectables_layout.addWidget(detectable)
@@ -157,11 +175,11 @@ class UnderwatchTab(QWidget):
             self.detectable = detectable
 
             layout = QGridLayout(self)
-            layout.setContentsMargins(0,0,0,0)
+            layout.setContentsMargins(0, 0, 0, 0)
             mininum_height = 40
 
-            name  = detectable[0]
-            label = QLabel(text = name, parent = self)
+            name = detectable[0]
+            label = QLabel(text=name, parent=self)
             label.setMinimumHeight(mininum_height)
 
             image = QLabel("", self)
@@ -182,7 +200,7 @@ class UnderwatchTab(QWidget):
             combo_box.setMinimumWidth(200)
             combo_box.addItem("Momentary Points")
             combo_box.addItem("Points Per Second")
-            if (detectable[1]["Duration"] != 1):
+            if detectable[1]["Duration"] != 1:
                 combo_box.addItem("Points Over Duration")
             combo_box.setCurrentIndex(detectable[1]["Type"])
             combo_box.currentIndexChanged.connect(self.update_points_type)
@@ -195,16 +213,17 @@ class UnderwatchTab(QWidget):
             layout.setColumnStretch(1, 1)
 
         def update_points(self, value):
-            self.detectable[1]["Points"] = value;
+            self.detectable[1]["Points"] = value
 
         def update_points_type(self, value):
-            self.detectable[1]["Type"] = value;
+            self.detectable[1]["Type"] = value
 
     def update(self):
-        if (self.score_input_box.hasFocus() == False):
+        if self.score_input_box.hasFocus() == False:
             self.score_input_box.blockSignals(True)
             self.score_input_box.setValue(int(self.computer_vision.score_over_time))
             self.score_input_box.blockSignals(False)
+
 
 class Worker(QThread):
     def __init__(self, funtion):
@@ -214,20 +233,28 @@ class Worker(QThread):
     def run(self):
         asyncio.run(self.function())
 
+
 def set_image_to_label(image, label):
-    #h, w, ch = 0
-    if(len(image.shape) == 2):
+    # h, w, ch = 0
+    if len(image.shape) == 2:
         h, w = image.shape
         ch = 1
     else:
         h, w, ch = image.shape
     bytes_per_line = ch * w
-    convert_to_Qt_format = QImage(image.data, w, h, bytes_per_line, QImage.Format_BGR888)
+    convert_to_Qt_format = QImage(
+        image.data, w, h, bytes_per_line, QImage.Format_BGR888
+    )
     width = label.width()
     heigth = label.height()
     qImage = convert_to_Qt_format.scaled(width, heigth, Qt.KeepAspectRatio)
     label.setPixmap(QPixmap(qImage))
 
-app = QApplication(sys.argv)
-app_window = GUI()
-sys.exit(app.exec())
+
+if __name__ == "__main__":
+    try:
+        app = QApplication(sys.argv)
+        app_window = GUI()
+        sys.exit(app.exec())
+    except Exception as e:
+        logging.critical("Application crashed: %s", str(e))
